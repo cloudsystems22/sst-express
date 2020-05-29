d516b1e16e674f57a9f0f345a30cba6941c8a5c6
cloudsystems22 <cloudsystems22@gmail.com>
2020-05-29 18:25:25 -0300

    Construçao dos setores

--- controllers/setoresController.js
+++ controllers/setoresController.js
@@ -1,11 +1,18 @@
+let { sequelize,  Usuario, Clientes, Setores } = require('../models');
 const setoresController = {
     index:async(req, res) =>{
 
     },
     create:async(req, res) =>{
+        let { setores, descricao, num_func_m, num_func_f, clientes_id } = req.body;
+        let setor = await Setores.create({setores, descricao, num_func_m, num_func_f, clientes_id });
+        res.send(setor);
 
     },
     update:async(req,res) =>{
+        let { id, setores, descricao, num_func_m, num_func_f, clientes_id } = req.body;
+        let setor = await Setores.update({setores, descricao, num_func_m, num_func_f, clientes_id, where: { id } });
+        res.send(setor);
 
     },
     delete:async(req, res) =>{
